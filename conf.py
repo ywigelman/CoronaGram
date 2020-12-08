5d2b5c387aa0a7665e08658ea0a0b16740622d4e
Yoav <ywigelman@gmail.com>
2020-12-09 01:09:38 +0200

    cli - adding arg

--- conf.py
+++ conf.py
@@ -61,7 +61,7 @@ INSTAGRAM_SUBMIT_TAG = "//button[@type=\"submit\"]"
 INSTAGRAM_NOT_NOW_BUTTON = "//button[text()='Not Now']"
 LOGIN_PAGE_WAIT = 3
 
-# constants with default values for instanting DRIVER object
+# constants with default values for instancing DRIVER object
 DEFAULT_IMPLICIT_WAIT = 50
 DEFAULT_BROWSER = 'CHROME'
 DEFAULT_EXECUTABLE = None
@@ -88,12 +88,12 @@ DEFAULT_STOP_CODE = None
 SCROLL_2_BOTTOM = 'window.scrollTo(0, document.body.scrollHeight);'  # java scrip command for scrolling to page bottom
 SCROLL_HEIGHT = 'return document.body.scrollHeight'  # java scrip command for getting scroll height
 
-# constants with default values for instanting PostScraper object
+# constants with default values for instancing PostScraper object
 DEFAULT_BATCH_SIZE = 50
 DEFAULT_POST_LIMIT = np.inf
-
 POST_LENGTH_TO_COMMIT = 10
 
+# constants for DB connections
 DB_NAME = 'itc1'
 DB_HOST_NAME = "localhost"
 DB_USER_NAME = "root"
@@ -106,7 +106,8 @@ X_RAPID_API_HOST = 'google-translate1.p.rapidapi.com'
 URL_LANGUAGE = 'https://google-translate1.p.rapidapi.com/language/translate/v2/detect'
 URL_TRANSLATION = 'https://google-translate1.p.rapidapi.com/language/translate/v2'
 TARGET_LANG = 'en'
-
-
 SENTIMENT_SUBSCRIPTION_KEY = "d2d3dc2b2fc94b64982dc912169f0a85"
 URL_SENTIMENT = 'https://textanalyticsitc.cognitiveservices.azure.com//text/analytics/v3.0/sentiment'
+
+# general CLI constants
+MAX_ENRICH = 0
